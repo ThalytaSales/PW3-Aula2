343799aa574b4ef47af99a3899a23588f4574e4b
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-08-02 20:34:18 -0300

    Exercicio 1 - TypeScript

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var notal = 10;
+var nota2 = 5;
+var nota3 = 9;
+var media = (notal + nota2 + nota3) / 3;
+console.log("A media é " + media);
+if (media >= 7) {
+    console.log("APROVADO VOCÊ É OTIMO");
+}
+else {
+    console.log("REPROVADO QUE MERDAAA");
+}
---
+++ exercicio1.ts
@@ -0,0 +1,14 @@
+ let notal: number = 10
+ let nota2: number = 5
+ let nota3: number = 9
+
+ let media: number = (notal+nota2+nota3) / 3
+
+ console.log("A media é " + media)
+
+ if(media>= 7){
+     console.log("APROVADO VOCÊ É OTIMO")
+ }else{
+     console.log("REPROVADO QUE MERDAAA");
+     
+ }
