55872a88f93f638d44cb2d3c13819977193f99a2
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-08-02 22:33:36 -0300

    exercicio 3/2

---
+++ dist/exercicio3.js
@@ -0,0 +1,9 @@
+var n1 = 10;
+var n2 = 5;
+var n3 = 9;
+//let media: number = (notal+nota2+nota3) / 3
+//let fmedia = function(notal:number,nota2:number,nota3:number): number {
+//    return (notal+nota2+nota3) / 3
+//}
+var fmedia = function (notal, nota2, nota3) { return (notal + nota2 + nota3) / 3; };
+console.log("A media é " + fmedia(n1, n2, n3));
---
+++ exercicio3.ts
@@ -0,0 +1,14 @@
+let n1: number = 10
+let n2: number = 5
+let n3: number = 9
+
+//let media: number = (notal+nota2+nota3) / 3
+
+//let fmedia = function(notal:number,nota2:number,nota3:number): number {
+//    return (notal+nota2+nota3) / 3
+//}
+
+let fmedia = (notal: number,nota2:number,nota3):number => (notal+nota2+nota3) / 3
+
+
+console.log("A media é " + fmedia(n1,n2,n3)) 
