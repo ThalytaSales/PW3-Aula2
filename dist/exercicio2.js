187c02a48b3dda04282e70d0ae18481da1837138
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-08-02 21:50:45 -0300

    exercicio 2

---
+++ dist/exercicio2.js
@@ -0,0 +1,10 @@
+var competidores = 10;
+var qntFolhas = 90;
+var folhasCC = 10;
+var resultado = (competidores * folhasCC);
+if (resultado > qntFolhas) {
+    console.log("NÃO");
+}
+else {
+    console.log("SIM");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,12 @@
+let competidores: number = 10
+let qntFolhas: number = 90
+let folhasCC: number = 10
+
+let resultado: number = (competidores * folhasCC)
+
+if (resultado> qntFolhas ){
+    console.log("NÃO")
+}else{
+    console.log("SIM");
+
+}
